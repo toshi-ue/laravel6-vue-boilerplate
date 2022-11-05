f762079fbf7a62fc9c76c13bb0d681f355233898
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-11-05 15:07:19 +0900

    サンプルを追加

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,5 +1,6 @@
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Sample from "./views/Sample.vue";
 
 export default new Router({
     mode: 'history',
@@ -7,5 +8,10 @@ export default new Router({
         path: '/',
         name: 'home',
         component: Home
-    },]
+    },
+    {
+        path: '/after_loggined_path',
+        name: 'sample',
+        component: Sample
+    }]
 })
---
+++ resources/js/views/Sample.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Sample</h1>
+        <p>path is after_loggined_path.</p>
+    </div>
+</template>
